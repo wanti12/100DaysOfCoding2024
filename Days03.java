0e4804adb89e321fb6db4799538e8e8e474ca95a
wanti12 <wantiwanti156@gmail.com>
2024-09-04 20:05:02 +0800

    Days03

---
+++ Days03.java
@@ -0,0 +1,14 @@
+package days03;
+
+public class Days03 {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t------biodata singkat------");
+        System.out.println("nama \t\t\t: wanti");
+        System.out.println("tempat/tanggal lahir\t: mamuju, 16 juni 2005");
+        System.out.println("alamat \t\t\t: papalang");
+        System.out.println("prodi\t\t\t: informatika");
+
+    }
+
+}
