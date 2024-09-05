4f51565a23efba3665b771a2c0dcdc0fed65897f
wanti12 <wantiwanti156@gmail.com>
2024-09-05 19:31:39 +0800

    Days04printdanprintf

---
+++ Days04printdanprintf.java
@@ -0,0 +1,15 @@
+package days04;
+
+public class Days04printdanprintf {
+    public static void main(String[] args) {
+        String nama="wanti";
+        int umur =19;
+        System.out.println("contoh penggunaan print:");
+        System.out.print("nama: " +nama+" ");
+        System.out.print("umur: "+umur);
+        
+        System.out.println("\n\ncontoh penggunaan printf:");
+        System.out.printf("nama saya %s saat ini saya berusia %d tahun",nama,umur);
+        System.out.println();
+    }
+}
