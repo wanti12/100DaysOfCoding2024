e59a7f953dce4ac1237bf3036c4e4808418259e1
wanti12 <wantiwanti156@gmail.com>
2024-09-07 19:03:33 +0800

    Days06DeklarasiVariabel

---
+++ Days06DeklarasiVariabel.java
@@ -0,0 +1,23 @@
+package days06DeklarasiVariabel;
+
+public class Days06DeklarasiVariabel {
+
+    public static void main(String[] args) {
+        byte iniByte;
+        short iniShort;
+        int iniint;
+        float inifloat;
+        long inilong;
+        double inidouble;
+        boolean intrue;
+        char inchar;
+        String iniString;
+        int a[];
+        
+        /*pendeklarasian variabel juga bisa dilakukan dengan lebih dari 
+        satu variabel dengan satu tipe data contoh:*/
+        int i1,i2,i3,i4;
+        String s1,s2,s3,s4,s5,s7; //dts.
+        
+    }
+}
