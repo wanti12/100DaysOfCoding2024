c852dc9874b63b6174a9d5a034eeaec3c35e8a97
wanti12 <wantiwanti156@gmail.com>
2024-09-09 19:13:05 +0800

    Days08UpdateNilai

---
+++ Days08UpdateNilai.java
@@ -0,0 +1,14 @@
+package days08UpdateNilai;
+
+public class Days08UpdateNilai {
+    public static void main(String[] args) {
+        int a=999;
+        System.out.println("nilai int = "+a);
+        a=1234;
+        System.out.println("setelah di update = "+a);
+        String b ="wanti";
+        System.out.println("sebelumu di update: "+b);
+        b="informatika";
+        System.out.println("setelah di update: "+b);
+    }
+}
