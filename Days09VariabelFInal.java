ff404a98af321f7fbfccdb6e32bfeaac3670443e
wanti12 <wantiwanti156@gmail.com>
2024-09-10 10:49:02 +0800

    Days09VariabelFinal

---
+++ Days09VariabelFInal.java
@@ -0,0 +1,14 @@
+package days09VariabelKonstan;
+
+public class Days09VariabelFInal {
+
+    public static void main(String[] args) {
+        final double phi = 3.14;
+        final int binner1 =0;
+        final int binner2 =1;
+        System.out.println("nilai phi= "+phi);   
+        System.out.println("nilai bilangan binner = "+binner1+" dan "+binner2);   
+        
+        
+    }
+}
