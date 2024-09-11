4846de2f89fc305854594954543b66f31df9e53a
wanti12 <wantiwanti156@gmail.com>
2024-09-11 15:10:20 +0800

    Days10Byte

---
+++ Days10Byte.java
@@ -0,0 +1,21 @@
+package days10TipeDatabyte;
+
+public class Days10Byte {
+
+    public static void main(String[] args) {
+        byte val1 = -127;
+        byte val2 = -100;
+        byte val3 = -91;
+        byte val4 = 50;
+        byte val5 = 89;
+        byte val6 = 99;
+        byte val7 = 1;
+        byte val8 = 10;
+        byte val9 = 19;
+        byte val10 = 127;
+
+        System.out.printf("value 1= %d\nvalue 2= %d\nvalue 3= %d\nvalue 4= %d\nvalue 5= %d"
+                + "\nvalue 6= %d\nvalue 7= %d\nvalue 8= %d\nvalue 9= %d\nvalue 10= %d\n", 
+                val1, val2, val3, val4, val5, val6, val7, val8, val9, val10);
+    }
+}
