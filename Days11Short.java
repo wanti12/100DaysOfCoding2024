264709c7efd754abeaf84afb429dae2d7cb1681c
wanti12 <wantiwanti156@gmail.com>
2024-09-12 17:18:23 +0800

    Days11short

---
+++ Days11Short.java
@@ -0,0 +1,31 @@
+package days11TipeDataShort;
+
+public class Days11Short {
+
+    public static void main(String[] args) {
+        short val1 = 128;
+        short val2 = 500;
+        short val3 = 2839;
+        short val4 = 12345;
+        short val5 = 9090;
+        short val6 = 9235;
+        short val7 = 1049;
+        short val8 = 3882;
+        short val9 = 392;
+        short val10 = 9939;
+        short val11 = 28023;
+
+        System.out.println("short value 1=" + val1);
+        System.out.println("short value 2=" + val2);
+        System.out.println("short value 3=" + val3);
+        System.out.println("short value 4=" + val4);
+        System.out.println("short value 5=" + val5);
+        System.out.println("short value 6=" + val6);
+        System.out.println("short value 7=" + val7);
+        System.out.println("short value 8=" + val8);
+        System.out.println("short value 9=" + val9);
+        System.out.println("short value 10=" + val10);
+        System.out.println("short value 11=" + val11);
+       
+    }
+}
