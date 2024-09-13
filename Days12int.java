9fd96fab58ce458106b7cbe8e2d5763baf80c38b
Wanti <147162518+wanti12@users.noreply.github.com>
2024-09-13 17:46:30 +0800

    Update Days12int.java

--- Days12int.java
+++ Days12int.java
@@ -3,31 +3,31 @@ package days12int;
 public class Days12int {
 
     public static void main(String[] args) {
-        int merah1;
-        int merah2;
-        int merah3;
-        int merah4;
-        int merah5;
-        int merah6;
-        merah1 = 7;
-        merah2 = 5;
-        merah3 = 9;
-        merah4 = 1;
-        merah5 = 3;
-        merah6 = 27;
-        System.out.printf("Bilangan Ganjil = %d, %d, %d, %d, %d, %d\n", merah1, merah2, merah3, merah4, merah5, merah6);
+        int merah;
+        int putih;
+        int maroon;
+        int hijau;
+        int hitam;
+        int coklat;
+        merah = 7;
+        putih = 5;
+        maroon = 9;
+        hijau = 1;
+        hitam = 3;
+        coklat = 27;
+        System.out.printf("Bilangan Ganjil = %d, %d, %d, %d, %d, %d\n", merah, putih, maroon, hijau, hitam, coklat);
 
-        int hijau1 = 8;
-        int hijau2 = 10;
-        int hijau3 = 4;
-        int hijau4 = 100;
-        System.out.println("Bilangan Genap = " + hijau1 + ", " + hijau2 + ", " + hijau3 + ", " + hijau4 );
+        int kuning = 8;
+        int ungu = 10;
+        int magenta = 4;
+        int oranye = 100;
+        System.out.println("Bilangan Genap = " + kuning + ", " + ungu + ", " + magenta + ", " + oranye );
 
-        int biru1 = 13;
-        int biru2 = 23;
-        int biru3 = 5;
-        int biru4 = 11;
-        System.out.printf("Bilangan Prima = %d, %d, %d, %d\n", biru1, biru2, biru3, biru4);
+        int cream = 13;
+        int emas = 23;
+        int silver = 5;
+        int peach = 11;
+        System.out.printf("Bilangan Prima = %d, %d, %d, %d\n", cream, emas, silver, peach);
 
     }
 }
