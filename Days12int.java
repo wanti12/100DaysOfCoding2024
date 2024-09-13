e49b0654351f33b8fab9203b8f0a9ae29f4bb1d9
wanti12 <wantiwanti156@gmail.com>
2024-09-13 17:02:10 +0800

    Days12int

---
+++ Days12int.java
@@ -0,0 +1,33 @@
+package days12int;
+
+public class Days12int {
+
+    public static void main(String[] args) {
+        int merah1;
+        int merah2;
+        int merah3;
+        int merah4;
+        int merah5;
+        int merah6;
+        merah1 = 7;
+        merah2 = 5;
+        merah3 = 9;
+        merah4 = 1;
+        merah5 = 3;
+        merah6 = 27;
+        System.out.printf("Bilangan Ganjil = %d, %d, %d, %d, %d, %d\n", merah1, merah2, merah3, merah4, merah5, merah6);
+
+        int hijau1 = 8;
+        int hijau2 = 10;
+        int hijau3 = 4;
+        int hijau4 = 100;
+        System.out.println("Bilangan Genap = " + hijau1 + ", " + hijau2 + ", " + hijau3 + ", " + hijau4 );
+
+        int biru1 = 13;
+        int biru2 = 23;
+        int biru3 = 5;
+        int biru4 = 11;
+        System.out.printf("Bilangan Prima = %d, %d, %d, %d\n", biru1, biru2, biru3, biru4);
+
+    }
+}
