92d2f4981d88c116709649993289bfc3447ac5c4
wanti12 <wantiwanti156@gmail.com>
2024-09-15 18:33:37 +0800

    Days14long

---
+++ Days14long.java
@@ -0,0 +1,25 @@
+package days14long;
+
+public class Days14long {
+
+    public static void main(String[] args) {
+        long value1, value2, value3, value4, value5, value6, value7, value8, value9, value10;
+        value1 = 1000;
+        value2 = 2000;
+        value3 = 3000;
+        value4 = 4000;
+        value5 = 5000;
+        value6 = 6000;
+        value7 = 7000;
+        value8 = 8000;
+        value9 = 9000;
+        value10 = 10000;
+        System.out.printf("nilai ke-1 = %d\nnilai ke-2 = %d\nnilai ke-3 = %d\nnilai ke-4 = %d\nnilai ke-5 = %d\nnilai ke6 = %d\nnilai ke-7 = %d\nnilai ke-8 = %d\nnilai ke-9 = %d\nnilai ke-10 = %d", value1, value2, value3, value4, value5, value6, value7, value8, value9, value10);
+
+        long value11 = 20000;
+        long value12 = 30000;
+        long value13 = 40000;
+        long value14 = 50000;
+        System.out.println("\nnilai ke-11 = " + value11 + "\nnilai ke-12 = " + value12 + "\nnilai ke-13 = " + value13 + "\nnilai ke-14 = " + value14);
+    }
+}
