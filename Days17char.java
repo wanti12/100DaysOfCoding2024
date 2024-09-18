57ba9347c30a8a675dd43e1af7c3239b6ae1d85c
wanti12 <wantiwanti156@gmail.com>
2024-09-18 19:20:59 +0800

    Days17char

---
+++ Days17char.java
@@ -0,0 +1,43 @@
+package days17;
+
+public class Days17char {
+
+    public static void main(String[] args) {
+        char a, b, c, d, e, f, g, h, i;
+        a = 'W';
+        b = 'A';
+        c = 'N';
+        d = 'T';
+        e = 'I';
+        f = '2';
+        g = '0';
+        h = '2';
+        i = '3'; 
+        System.out.println("nilai char jika di cetak dengan String : " + a + b + c + d + e + " " + f + g + h + i);
+        System.out.println("nilai char jika di cetak tanpa String");
+        System.out.println(a + b + c + d + e +  f + g + h + i);
+
+        a = 72;
+        b = 69;
+        c = 76;
+        d = 76;
+        e = 79;
+        f = 50;
+        g = 48;
+        h = 50;
+        i = 52;
+        System.out.println("nilai char setelah di update");
+        System.out.printf("%s", a);
+        System.out.printf("%s", b);
+        System.out.printf("%s", c);
+        System.out.printf("%s", d);
+        System.out.printf("%s ", e);
+        System.out.printf("%s", f);
+        System.out.printf("%s",g);
+        System.out.printf("%s", h);
+        System.out.printf("%s", i);
+        System.out.println("");
+       
+
+    }
+}
