0ace85a291ba783b0ac06d6fdd41cc8e3f82de14
wanti12 <wantiwanti156@gmail.com>
2024-09-19 19:21:13 +0800

    Days18

---
+++ Days18boolean.java
@@ -0,0 +1,34 @@
+package days18;
+
+public class Days18boolean {
+
+    public static void main(String[] args) {
+        boolean value1, value2;
+        value1 = true;
+        value2 = false;
+        System.out.printf("nilai 1 = %b\nnilai 2 = %b", value1, value2);
+        System.out.println("");
+
+        boolean a = 1 < 3;
+        System.out.println("apakah 1 lebih kecil dari 3? "+a);
+        boolean b = 3 < 2;
+        System.out.println("apakah 3 lebih kecil dari 2? "+b);
+        boolean c = 5 < 1;
+        System.out.println("apakah 5 lebih kecil dari 1? "+c);
+        boolean d = 11 == 20;
+        System.out.println("apakah 11 sama dengan 20? "+d);
+        boolean e = 40 == 40;
+        System.out.println("apakah 40 sama dengan 40? "+e);
+        boolean f = 6 > 3;
+        System.out.println("apakah 6 lebih besar dari 3? "+f);
+        boolean g = 5 > 1;
+        System.out.println("apakah 5 lebih besar dari 1? "+g);
+        boolean h = 90 > 55;
+        System.out.println("apakah 90 lebih besar dari 55? "+h);
+        boolean i = 77 > 99;
+        System.out.println("apakah 77 lebih besar dari 99? "+i);
+        boolean j = 22 > 11;
+        System.out.println("apakah 22 lebih besar dari 11? "+j);
+
+    }
+}
