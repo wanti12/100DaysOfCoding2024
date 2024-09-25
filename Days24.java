2ffaf6225a85c7df7d02a219ecf21f8005582447
wanti12 <wantiwanti156@gmail.com>
2024-09-25 21:05:10 +0800

    Days24

---
+++ Days24.java
@@ -0,0 +1,35 @@
+package days24;
+
+public class Days24 {
+
+    public static void main(String[] args) {
+        Float a, b, c, d, e;
+        a = 7000.9993f;
+        b = 9000.97f;
+        c = 193.822f;
+        d = 2289.453f;
+        e = 345.123f;
+        System.out.printf("nilai a = %f\nnilai b =%f\nnilai c = %f\nnilai d = %f\nnilai e = %f\n",a,b,c,d,e);
+        
+        a=500.0f;
+        b=400.12f;
+        c=900.70f;
+        d=200.20f;
+        e=50.2f;
+        
+        System.out.println("nilai variabel setelah di update");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println("e = "+e);
+        
+        System.out.println("nilai variabel setelah dibagi 2");
+        System.out.println("a = "+a/2);
+        System.out.println("b = "+b/2);
+        System.out.println("c = "+c/2);
+        System.out.println("d = "+d/2);
+        System.out.println("e = "+e/2);
+        
+    }
+}
