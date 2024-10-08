1e5e36a91b790e18bc4b4d0ca1aac3da54627328
wanti12 <wantiwanti156@gmail.com>
2024-10-08 19:28:04 +0800

    Days37

---
+++ days37.java
@@ -0,0 +1,31 @@
+package days37;
+
+public class days37 {
+
+    public static void main(String[] args) {
+        byte a = 21;
+        System.out.printf("nilai byte = %d%n", a);
+        short b = 23;
+        System.out.printf("nilai short = %d%n", b);
+        int c = 30;
+        System.out.printf("nilai int = %d%n", c);
+        c += 90;
+        System.out.printf("setelah di tambah = %d%n", c);
+        long d = 900;
+        System.out.printf("nilai long = %d%n", d);
+        d += 200;
+        System.out.printf("setelah di tambah= %d%n", d);
+        float e = 2098.22f;
+        System.out.printf("nilai float = %.2f%n", e);
+        e -= 20.22;
+        System.out.printf("setelah di kurangi = %.2f%n", e);
+        double f = 99987.900;
+        System.out.printf("nilai double = %.3f%n", f);
+        f -= 909.99;
+        System.out.printf("setelah di kurang= %.3f%n", f);
+        String g = "wanti";
+        System.out.printf("nilai String = %s%n", g);
+        g += "unsubar";
+        System.out.printf("setelah di tambah = %s%n", g);
+    }
+}
