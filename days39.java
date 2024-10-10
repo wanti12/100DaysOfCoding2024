4008f8a8bd1720b9ecd7283f3208cc405d6e92aa
wanti12 <wantiwanti156@gmail.com>
2024-10-10 17:56:31 +0800

    Days39

---
+++ days39.java
@@ -0,0 +1,30 @@
+package days39;
+
+import java.util.Scanner;
+
+public class days39 {
+
+    public static void main(String[] args) {
+        //mengambil nilai dari inputan
+        Scanner wa = new Scanner(System.in);
+        System.out.println("masukkan angak pertama ");
+        int a = wa.nextInt();
+        System.out.println("masukkan angka kedua ");
+        int b = wa.nextInt();
+
+        //mencari sisa bagi
+        int c = a % b;
+        System.out.printf("sisa bagi %d / %d = %d%n", a, b, c);
+        
+        //operasi penugasan,perkalian,dan sisa bagis
+        System.out.println("masukkan angka ketiga ");
+        int d = wa.nextInt();
+        int e = d * c;
+        System.out.printf("%d x %d = %d%n", d, c, e);
+        double f = e % 2;
+        System.out.printf("sisa bagi %d / 2 = %f%n", e, f);
+
+        String g =String.valueOf(f);
+        System.out.println("nilai f dalam bentuk string = "+g);
+    }
+}
