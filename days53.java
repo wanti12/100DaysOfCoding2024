deb18f503f3deb38d40723a5175f102ad1c762fb
wanti12 <wantiwanti156@gmail.com>
2024-10-24 19:52:50 +0800

    Days53

---
+++ days53.java
@@ -0,0 +1,15 @@
+package days53;
+
+import java.util.Scanner;
+
+public class days53 {
+
+    public static void main(String[] args) {
+        Scanner wa = new Scanner(System.in);
+        System.out.println("masukkan batas");
+        int batas = wa.nextInt();
+        
+        
+    }
+
+}
