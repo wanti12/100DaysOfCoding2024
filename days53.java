0dfaf6fc69a8d596edcca20f92ccea5d2843529c
Wanti <147162518+wanti12@users.noreply.github.com>
2024-10-25 10:34:31 +0800

    Update days53.java

--- days53.java
+++ days53.java
@@ -8,8 +8,27 @@ public class days53 {
         Scanner wa = new Scanner(System.in);
         System.out.println("masukkan batas");
         int batas = wa.nextInt();
-        
-        
-    }
 
+        double sum = 0;
+        double data = 0;
+
+        for (int i = 2; i < batas; i++) {
+            boolean prima = true;
+           
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+            if (prima) {
+                System.out.println(i);
+                sum += i;
+                data++;
+            }
+        }
+        double avg = sum / data;
+        System.out.println("sum = " + sum);
+        System.out.println("avg = " + avg);
+    }
 }
