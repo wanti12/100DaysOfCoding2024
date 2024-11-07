d1d923e51d9d206b72b1dbdb9f02c16ca846ecf3
wanti12 <wantiwanti156@gmail.com>
2024-11-07 18:24:55 +0800

    Days67

---
+++ days67.java
@@ -0,0 +1,25 @@
+package days67;
+
+public class days67 {
+
+    public static void main(String[] args) {
+        System.out.println("===pola persegi===");
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n===pola persegi panjang===");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+
+}
