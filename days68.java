a8343004905b057cf262bd51ed1d0d9789c05428
Wanti <147162518+wanti12@users.noreply.github.com>
2024-11-08 19:01:17 +0800

    days68.java

--- days68.java
+++ days68.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class days68 {
 
+    static int p;
+    static int l;
+
     public static void main(String[] args) {
         Scanner wa = new Scanner(System.in);
         System.out.print("input panjang: ");
-        int p = wa.nextInt();
+        p = wa.nextInt();
         System.out.print("input lebar: ");
-        int l = wa.nextInt();
-        hitungLuas(p, l);
-        hitungKeliling(p, l);
+        l = wa.nextInt();
+        hitungLuas();
+        hitungKeliling();
 
     }
 
-    public static int hitungLuas(int p, int l) {
+    public static int hitungLuas() {
         int luas = p * l;
         System.out.printf("luas persegi panjang = %d cm%n", luas);
         return luas;
     }
 
-    public static int hitungKeliling(int p, int l) {
+    public static int hitungKeliling() {
         int keliling = 2 * (p + l);
         System.out.printf("keliling persegi panjang = %d cm%n", keliling);
         return keliling;
