1f5c28181059fa634e48abcbd046b7057940cadf
Wanti <147162518+wanti12@users.noreply.github.com>
2024-12-04 22:40:34 +0800

    days93.java

--- days93.java
+++ days93.java
@@ -1,28 +1,5 @@
 package days93;
-<<<<<<< HEAD
 import java.util.Scanner;
-public class days93 {
-    public static void main(String[] args) {
-        Scanner wa = new Scanner (System.in);
-        System.out.print("input angka: ");
-        int input = wa.nextInt();
-        
-        System.out.println("");
-        for (int i = 0; i < input; i++) {
-            for (int j = 0; j < input - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
-    }
-    
-=======
-
-import java.util.Scanner;
-
 public class days93 {
 
     public static void main(String[] args) {
@@ -42,5 +19,4 @@ public class days93 {
             System.out.println();
         }   
     }
->>>>>>> 9bd137ede3e9b648024e34149a574e313464b69a
 }
