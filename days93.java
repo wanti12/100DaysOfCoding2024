7ffabc014d50c0568cadb5770e05918f8603a34d
wanti12 <wantiwanti156@gmail.com>
2024-12-04 16:56:21 +0800

    days93

---
+++ days93.java
@@ -0,0 +1,21 @@
+package days93;
+import java.util.Scanner;
+public class days93 {
+    public static void main(String[] args) {
+        Scanner wa = new Scanner (System.in);
+        System.out.print("input angka: ");
+        int input = wa.nextInt();
+        
+        System.out.println("");
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+    
+}
